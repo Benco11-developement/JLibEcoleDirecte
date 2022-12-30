a4ede6ee34e08de9fbeb3d38c8577e27dab13920
Benco11-developement <anjuforwap@gmail.com>
2022-12-30 23:16:41 +0100

    Reset all, migrate to gradle 7.6, switch to java 17 and modify gradle build configuration

--- src/main/java/fr/benco11/jlibecoledirecte/exceptions/EcoleDirecteUnknownConnectionException.java
+++
@@ -1,9 +0,0 @@
-package fr.benco11.jlibecoledirecte.exceptions;
-
-public class EcoleDirecteUnknownConnectionException extends Exception {
-	private static final long serialVersionUID = -4682370957544062464L;
-	@Override
-	public String getMessage() {
-		return "Une erreur inconnue s'est produite";
-	}
-}
